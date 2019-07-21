058d3cd1bcf7ba958d6b08680d20d781435a428d
SAMBIT KUMAR MAJHI <majhisambit2@gmail.com>
2019-07-21 11:15:17 +0530

    updtare

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,31 +5,29 @@ import (
 	"strconv"
 )
 
-var (
-	p  int    = 4
-	uu string = "hellllo"
-)
-
 func main() {
 	fmt.Println("helllo")
 	var i int
 
 	i = 30
 	fmt.Println(i)
-	var j int = 60
+	var j int
+	j = 60
 	fmt.Println(j)
 	k := 70
 	fmt.Println(k)
 	fmt.Printf("%v, %T\n", i, i)
 	fmt.Printf("%v, %T\n", j, j)
 	fmt.Printf("%v, %T\n", k, k)
-	fmt.Println(p)
-	var p int = 5
+
+	var p int
+	p = 5
 	//p := 6 // not posiible as p already exists
 	p = 7 // possible
 	fmt.Println(p)
 
-	var t int = 77
+	var t int
+	t = 77
 	fmt.Printf("%v, %T\n", t, t)
 
 	var r float64
@@ -41,4 +39,5 @@ func main() {
 	fmt.Printf("%v, %T\n", q, q)
 	q = strconv.Itoa(t) //will print exact value of converted data
 	fmt.Printf("%v, %T\n", q, q)
+
 }
